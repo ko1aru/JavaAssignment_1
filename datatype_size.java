34ef3bc8892a60a57f3a3319ef1e83263902c40d
ko1aru <83403149+ko1aru@users.noreply.github.com>
2021-09-05 23:38:08 +0530

    Add files via upload

---
+++ datatype_size.java
@@ -0,0 +1,13 @@
+package datatype_size;
+
+public class datatype_size {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("int: "+ (Integer.SIZE/8) +" bytes");
+		System.out.println("float: "+ (Float.SIZE/8) +" bytes");
+		System.out.println("long: "+ (Long.SIZE/8) +" bytes");
+		System.out.println("char: "+ (Character.SIZE/8) +" bytes");
+	}
+
+}
