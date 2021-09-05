30736d765cd4a554aaa257154cb8a5173726c1e7
ko1aru <83403149+ko1aru@users.noreply.github.com>
2021-09-05 23:43:58 +0530

    Add files via upload

---
+++ javaver.java
@@ -0,0 +1,14 @@
+
+public class javaver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java Version: "+System.getProperty("java.version"));
+		System.out.println("Java Runtime Version: "+System.getProperty("java.runtime.version"));
+		System.out.println("Java home: "+System.getProperty("java.home"));
+		System.out.println("Java Vendor: "+System.getProperty("java.vendor"));
+		System.out.println("Java Vendor URL: "+System.getProperty("java.vendor.url"));
+		System.out.println("Java Class Path: "+System.getProperty("java.class.path"));
+	}
+
+}
