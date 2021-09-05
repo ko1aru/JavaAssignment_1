64e24914c5e9886c0016390acaa31913910baa59
ko1aru <83403149+ko1aru@users.noreply.github.com>
2021-09-05 23:18:20 +0530

    Add files via upload

---
+++ VariableDeclaration.java
@@ -0,0 +1,16 @@
+package var_declaration;
+
+public class VariableDeclaration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		double b=19.05;
+		float c=46.0F;
+		long d=936734674;
+		boolean e = false;
+		String f="declaration";
+		System.out.print("a="+a+ "\n" +"b="+b+ "\n" +"c="+c+ "\n" +"d="+d+ "\n" +"e="+e+ "\n" +"f="+f);
+	}
+
+}
